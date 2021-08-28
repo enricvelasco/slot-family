1c32409ede9b72f4e563ba9aea52806032b44f9c
enricvelasco <enric@woffu.com>
2021-08-28 23:39:36 +0200

    remove unused css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import React from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
